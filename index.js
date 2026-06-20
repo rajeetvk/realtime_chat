9f3f2663c3c4a8facc97d4678abfc29e8e2cfa90
Arjeet Singh <arjeet008.gzp@gmail.com>
2026-06-20 19:08:28 +0530

    feat(backend): implement socket.io database integration and jwt auth

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
-require('./config/db'); // Load and test the database connection
+const db = require('./config/db'); // Load and test the database connection
 const express = require("express");
 const app = express();
 const http = require('http');
 const path = require('path');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
+const jwt = require('jsonwebtoken');
+
 const io = new Server(server);
 
 // Middleware to parse incoming JSON data from forms
@@ -19,15 +21,39 @@ app.use(express.static(path.join(__dirname, 'public')));
 const authRoutes = require('./routes/auth');
 app.use('/auth', authRoutes);
 
+io.use((socket, next) => {
+    const token = socket.handshake.auth.token;
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodedUser) => {
+        if (err) {
+            return next(new Error("Authentication Error."))
+        }
+        socket.user = decodedUser;
+        next();
+
+    })
+})
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
-        io.emit('chat message', msg);
+    console.log('A user connected:', socket.user.username);
+
+    db.any('SELECT users.username, messages.text FROM messages JOIN users ON messages.user_id = users.id ORDER BY messages.created_at ASC LIMIT 50')
+        .then(history => {
+            socket.emit('chat history', history);
+        });
+
+    socket.on('chat message', async (msg) => {
+        try {
+            await db.none('INSERT INTO messages (user_id,text) VALUES ($1,$2)', [socket.user.id, msg]);
+
+            io.emit('chat message', { username: socket.user.username, text: msg });
+
+        }
+        catch (error) {
+            console.error('Error saving message:', error);
+        }
     });
 
     socket.on('disconnect', () => {
-        console.log('user disconnected');
+        console.log('User disconnected:', socket.user.username);
     });
 });
 
